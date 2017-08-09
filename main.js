d0a9d811753e56b5795fdf52c06f8ca24d8db5b6
Andrew Gibson <andrewgib14@gmail.com>
2017-08-09 14:26:19 -0500

    can search for artists now and formatted items

--- main.js
+++ main.js
@@ -7,3 +7,27 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+let button = document.querySelector("button")
+let input = document.querySelector("input");
+let grid = document.querySelector(".grid")
+let url = "https://itunes.apple.com/search?";
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  grid.innerHTML = "";
+  let searchText = input.value;
+
+  url = "https://itunes.apple.com/search?term=" + searchText + "&entity=song&limit=200";
+  axios.get(url).then(function (response) {
+    console.log(response);
+    let results = response.data.results;
+    for (let i = 0; i < results.length; i++) {
+      let thumbnail = results[i].artworkUrl100;
+      let bandName = results[i].artistName;
+      let songTitle = results[i].trackName;
+      grid.innerHTML += `<div class="results"><img src ="${thumbnail}"><p><span>Song: </span>${songTitle}</p><p><span>Artist: </span>${bandName}</p ></div > `
+    }
+  })
+
+})
