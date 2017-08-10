eabab1aef38e7c8d6b32f9b8c64effc40974e2c3
Andrew Gibson <andrewgib14@gmail.com>
2017-08-10 10:41:20 -0500

    got the music to play

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
 
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
 
-let button = document.querySelector("button")
+let button = document.querySelector("button");
 let input = document.querySelector("input");
-let grid = document.querySelector(".grid")
+let grid = document.querySelector(".grid");
+let currentTrack = document.querySelector(".music-player");
+let nowPLaying = document.querySelector(".nowPLaying");
 let url = "https://itunes.apple.com/search?";
 
 button.addEventListener("click", function (e) {
@@ -26,8 +20,16 @@ button.addEventListener("click", function (e) {
       let thumbnail = results[i].artworkUrl100;
       let bandName = results[i].artistName;
       let songTitle = results[i].trackName;
-      grid.innerHTML += `<div class="results"><img src ="${thumbnail}"><p><span>Song: </span>${songTitle}</p><p><span>Artist: </span>${bandName}</p ></div > `
+      let trackPreview = results[i].previewUrl;
+      grid.innerHTML += `<div class="results"><img id="track-${[i]}" value="${trackPreview}" src ="${thumbnail}"><p><span>Song: </span>${songTitle}</p><p><span>Artist: </span>${bandName}</p ></div > `;
     }
-  })
 
+    grid.addEventListener("click", function (e) {
+      let trackPrev = e.target.getAttribute('value');
+      nowPlaying = "";
+      nowPlaying.innerHTML = e.target.value;
+      currentTrack.setAttribute("src", trackPrev);
+
+    })
+  })
 })
